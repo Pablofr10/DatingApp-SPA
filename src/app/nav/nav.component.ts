f6188a55b99fd7f0d0270e48ffc22fde3626fc62
Pablo Rafael Oliveira de Araújo <pabloweb@outlook.com>
2020-07-23 00:58:32 -0300

    Adicionando Register

---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+  model: any = {};
+
+  constructor(private authServices: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  login() {
+    this.authServices.login(this.model).subscribe(x => {
+      console.log('Logged successful');
+    }, err => {
+      console.log('Failed to login');
+    });
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+  }
+}
